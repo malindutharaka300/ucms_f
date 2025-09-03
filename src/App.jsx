338d261a24f4f740fb87b454a8d377a902bd20be
malindutharaka300 <malindutharaka300@gmail.com>
2025-09-03 20:13:49 +0530

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from './components/auth/login';
 import Register from './components/auth/register';
 import Dashboard from './dashboard';
@@ -7,26 +7,22 @@ import Content from './components/course/contents';
 import Result from './components/result/result';
 import Assign from './components/assign/assign';
 
-function App() {
+export default function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/ucms_f">
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/register" element={<Register/>} />
 
         {/* Parent layout */}
         <Route path="/dashboard" element={<Dashboard />}>
-          {/* default page when visiting /dashboard */}
           <Route index element={<Navigate to="courses" replace />} />
-          {/* child routes are RELATIVE to /dashboard */}
           <Route path="courses" element={<Course />} />
           <Route path="content/:courseId" element={<Content />} />
           <Route path="result" element={<Result />} />
           <Route path="assign" element={<Assign />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
-
-export default App;
